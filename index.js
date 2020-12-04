d86d917b66e0c64f79d374c5dcc244af17607d7a
jugalkpatel <ptjimmy99@gmail.com>
2020-12-04 10:15:59 +0000

    feat: add level two and three questions

--- index.js
+++ index.js
@@ -47,6 +47,49 @@ const levelOneQuestions = [
   },
 ]
 
+// Level2
+const levelSecondQuestions = [
+  {
+    question: "Which Building card has biggest Range?",
+    answer: "xbow",
+    options: ["xbow", "mortar", 'inferno-tower', 'barbarian-hut']
+  },
+  {
+    question: "Which card in clash royale has most single-hit damage?",
+    answer: "Sparky",
+    options: ["Pekka", "Mega-Night", "Sparky"]
+  },
+  {
+    question: "Which Card has highest hit points in clash royale?",
+    answer: "golem",
+    options: ["pekka", "golem", "Giant-Skelly"]
+  },
+  {
+    question: "Which spell does most damage if ignored?",
+    answer: "gravy yard",
+    options: ['Royal Delivery', 'gravy yard', 'Mirror', 'barbarian barrel']
+  },
+  {
+    question: "which card has highest damage per second in game?",
+    answer: "Sparky",
+    options: ['Mega Minion', 'Prince', 'Mega Minion', 'Sparky']
+  },
+]
+
+// Level3
+const levelThreeQuestions = [
+  {
+    question: "Is rage spell affect all things inside it?",
+    options: undefined,
+    answer: true
+  },
+  {
+    question: "Is Healer Heels in Spell?",
+    options: undefined,
+    answer: true
+  }
+]
+
 // add score
 let score = 0;
 
