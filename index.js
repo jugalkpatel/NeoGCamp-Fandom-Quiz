0155481f3a1f09060afb5e506be0d71cc827b198
jugalkpatel <ptjimmy99@gmail.com>
2020-12-04 10:08:18 +0000

    feat: high scorers logic

--- index.js
+++ index.js
@@ -2,3 +2,29 @@
 let readlineSync = require('readline-sync');
 let chalk = require('chalk');
 
+// high score logic
+const highScorers = [
+  {
+    name: "king",
+    score: 10
+  },
+  {
+    name: "princess",
+    score: 10
+  }
+]
+
+// add score
+let score = 0;
+
+// Check for high Score
+function CheckhighScore(name, score) {
+  // console.log("High Score in Calling....");
+  for (let i = 0; i < highScorers.length; i++) {
+    if (score > highScorers[i].score) {
+      // console.log("Pushed to high score");
+      highScorers.push({ name: name, score: score });
+      break;
+    }
+  }
+}
